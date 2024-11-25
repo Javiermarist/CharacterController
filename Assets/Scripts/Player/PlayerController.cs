9218ce9f66c14b4eb51a63919e72f1a09016a8c6
Javiermarist <javiermarist@gmail.com>
2024-11-25 12:43:33 +0100

    Added camera

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -21,6 +21,12 @@ public class PlayerController : MonoBehaviour
     private Vector3 cameraForward;
     private Vector3 cameraRight;
 
+    // Fall
+    public bool isOnBigSlope = false;
+    public Vector3 hitNormal; // Necesitamos la normal para saber en que superficie estamos
+    public float slideVelocity;
+    public float slopeForceDown;
+
     void Start()
     {
         playerInput = GetComponent<PlayerInput>();
@@ -31,6 +37,7 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         Movement();
+        camDirection();
         SetGravity();
         player.Move(movePlayer * Time.deltaTime);
     }
@@ -53,11 +60,39 @@ public class PlayerController : MonoBehaviour
             /*movePlayer.y = fallVelocity;*/
         }
         movePlayer.y = fallVelocity;
+        SlideDown();
+    }
+
+    public void SlideDown()
+    {
+        isOnBigSlope = Vector3.Angle(Vector3.up, hitNormal) >= player.slopeLimit;
+        if (isOnBigSlope)
+        {
+            movePlayer.x += ((1f - hitNormal.y) * hitNormal.x) * slideVelocity;
+            movePlayer.z += ((1f - hitNormal.y) * hitNormal.z) * slideVelocity;
+            movePlayer.y = -slideVelocity;
+        }
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        hitNormal = hit.normal;
     }
 
     private void Movement()
     {
         Vector2 inputV2 = playerInput.actions["Move"].ReadValue<Vector2>();
+        input = new Vector3(inputV2.x, 0, inputV2.y);
+        input = Vector3.ClampMagnitude(input, 1);
+        //movePlayer = input.x * cameraRight + input.z * cameraForward;
+        movePlayer = input.x * cameraRight + input.z * cameraForward;
+        movePlayer *= moveSpeed;
+        player.transform.LookAt(player.transform.position + movePlayer);
+    }
+
+    public void OnMove(InputValue value)
+    {
+        Vector2 inputV2 = value.Get<Vector2>();
         input = new Vector3(inputV2.x, 0, inputV2.y);
         input = Vector3.ClampMagnitude(input, 1);
         movePlayer = input.x * cameraRight + input.z * cameraForward;
@@ -67,6 +102,13 @@ public class PlayerController : MonoBehaviour
 
     private void camDirection()
     {
+        cameraForward = mainCamera.transform.forward;
+        cameraRight = mainCamera.transform.right;
 
+        cameraForward.y = 0;
+        cameraRight.y = 0;
+
+        cameraForward = cameraForward.normalized;
+        cameraRight = cameraRight.normalized;
     }
 }
